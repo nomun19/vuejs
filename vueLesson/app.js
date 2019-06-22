7991f9aee4159e76b61e77be15a64efcc9a84b91
Nomundari Erdenebileg <namuna91nomuna@gmail.com>
2019-06-22 23:01:48 +0800

    vuejs lesson

---
+++ vueLesson/app.js
@@ -0,0 +1,41 @@
+
+Vue.use(vuelidate.default)
+
+new Vue({
+
+    el:'#app',
+
+          data(){
+            return {
+              form: {
+                name: null,
+
+                age:null,
+
+              }
+            }
+          },
+
+          validations :{
+            form: {
+              name: {
+                required: validators.required
+              },
+              age: {
+                required: validators.required,
+                integer: validators.integer,
+                between: validators.between(12,120)
+              }
+            }
+          },
+
+          methods: {
+            submitForm () {
+              if(this.$v.form.name.$invalid) {
+                console.log('📝 Form Submitted', this.form)
+              } else {
+                console.log(' ❌ Invalid Form')
+              }
+            }
+          },
+        }) 
